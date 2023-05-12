fb77ef6c3eeee46d2b24486c97061ffedea89f6d
ilonavarro <ilo.navarro+github@gmail.com>
2023-05-12 12:51:59 +0100

    Update Next 13.4.2, add footer finished page

--- src/app/page.js
+++ src/app/page.js
@@ -18,16 +18,29 @@ export default function Home() {
             />
           </div>
           <div className='w-1/2 flex flex-col items-center self-center'>
-            <AnimatedText className='text-6xl text-left'>
+            <AnimatedText className='!text-6xl !text-left'>
               Turning Vision Into Reality With Code And Design.
             </AnimatedText>
-            <p className='my-4 textbas font-medium'>
+            <p className='my-4 text-base font-medium'>
               As a skilled full-stack developer, I am dedicated to turning ideas into
               innovative web applications. Explore my latest projects and articles,
               showcasing my expertise in React.js and web development.
             </p>
-            <div>
-              <Link href='#'>My resume</Link>
+            <div className='flex items-center self-start mt-2'>
+              <Link
+                href='#'
+                className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold 
+                border-2 border-solid border-transparent
+                hover:bg-light hover:text-dark hover:border-dark '
+              >
+                My Resume
+              </Link>
+              <Link
+                href='/contact'
+                className='ml-4 text-lg font-medium capitalize text-dark underline'
+              >
+                Contact me
+              </Link>
             </div>
           </div>
         </div>
