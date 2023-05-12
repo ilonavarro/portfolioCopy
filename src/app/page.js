f8e773baf5f19fb109d87ff667381d83f9216aef
ilonavarro <ilo.navarro+github@gmail.com>
2023-05-12 01:06:31 +0100

    updates and index page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Layout from '@/components/Layout'
 import Image from 'next/image'
 import profilePic from '../../public/images/profile/developer-pic-1.png'
 import AnimatedText from '@/components/AnimatedText'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,15 +10,25 @@ export default function Home() {
       <Layout className='pt-0'>
         <div className='flex items-center justify-between w-full'>
           <div className='w-1/2'>
-            <Image src={profilePic} alt='Ilo Dev' className='w-full h-auto' />
+            <Image
+              src={profilePic}
+              alt='Ilo Dev'
+              className='w-full h-auto'
+              priority={true}
+            />
           </div>
-          <div className='w-1/2'>
-            <AnimatedText>Turning Vision Into Reality With Code And Design.</AnimatedText>
-            <p>
+          <div className='w-1/2 flex flex-col items-center self-center'>
+            <AnimatedText className='text-6xl text-left'>
+              Turning Vision Into Reality With Code And Design.
+            </AnimatedText>
+            <p className='my-4 textbas font-medium'>
               As a skilled full-stack developer, I am dedicated to turning ideas into
               innovative web applications. Explore my latest projects and articles,
               showcasing my expertise in React.js and web development.
             </p>
+            <div>
+              <Link href='#'>My resume</Link>
+            </div>
           </div>
         </div>
       </Layout>
--- src/components/AnimatedText/index.js
+++ src/components/AnimatedText/index.js
@@ -1,3 +1,32 @@
+'use client'
+import { motion } from 'framer-motion'
+
+const quote = {
+  initial: {
+    opacity: 1
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      staggerChildren: 0.08
+    }
+  }
+}
+
+const singleWord = {
+  initial: {
+    opacity: 0,
+    y: 50
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1
+    }
+  }
+}
 export default function AnimatedText({ children, className = '' }) {
   return (
     <>
@@ -6,15 +35,22 @@ export default function AnimatedText({ children, className = '' }) {
           'w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden'
         }
       >
-        <h1
+        <motion.h1
           className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
+          variants={quote}
+          initial='initial'
+          animate='animate'
         >
           {children.split(' ').map((word, index) => (
-            <span key={word + '-' + index} className={'inline-block'}>
+            <motion.span
+              key={word + '-' + index}
+              className={'inline-block'}
+              variants={singleWord}
+            >
               {word}&nbsp;
-            </span>
+            </motion.span>
           ))}
-        </h1>
+        </motion.h1>
       </div>
     </>
   )
