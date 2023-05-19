179d3830e105883dae9b6e6e9a3d28812c820997
ilonavarro <ilo.navarro+github@gmail.com>
2023-05-19 12:13:42 +0100

    update Next 13.4.3 and finished skill and skills

--- src/components/Skills/Skill.js
+++ src/components/Skills/Skill.js
@@ -1,22 +1,30 @@
 'use client'
 import { motion } from 'framer-motion'
 
-export default function Skill({ text, x, y }) {
+export default function Skill({ text, x, y, tooltip = '✨ You hover me!' }) {
   return (
-    <motion.div
-      className='flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-grab
+    <>
+      <motion.div
+        className='group flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-grab
       absolute'
-      whileHover={{
-        scale: 1.05,
-        textShadow: '0px 0px 4px rgb(44, 215, 254)',
-        boxShadow: '0px 0px 8px rgb(44, 215, 254)'
-      }}
-      initial={{ x: 0, y: 0 }}
-      whileInView={{ x: x, y: y }}
-      transition={{ duration: 1.5 }}
-      viewport={{ once: true }}
-    >
-      {text}
-    </motion.div>
+        whileHover={{
+          scale: 1.05,
+          textShadow: '0px 0px 4px rgb(44, 215, 254)',
+          boxShadow: '0px 0px 8px rgb(44, 215, 254)'
+        }}
+        initial={{ x: 0, y: 0 }}
+        whileInView={{ x: x, y: y }}
+        transition={{ duration: 1.5 }}
+        viewport={{ once: true }}
+      >
+        {text}
+        <span
+          className='absolute top-10 scale-0 transition-all rounded-full bg-red-950 p-2 text-xs text-light group-hover:scale-100 
+        text-center'
+        >
+          {tooltip}
+        </span>
+      </motion.div>
+    </>
   )
 }
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -5,6 +5,7 @@ export default function Skills() {
     <>
       <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
       <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
+        <Skill x='0vw' y='-22vw' text='Figma' tooltip='Used to create designs' />
         <Skill x='0vw' y='0vw' text='Web' />
         <Skill x='-5vw' y='-10vw' text='CSS' />
         <Skill x='-23vw' y='2vw' text='HTML' />
@@ -12,10 +13,14 @@ export default function Skills() {
         <Skill x='20vw' y='6vw' text='React JS' />
         <Skill x='-22vw' y='-15vw' text='Next JS' />
         <Skill x='15vw' y='-12vw' text='Wordpress' />
-        <Skill x='30vw' y='-5vw' text='Web Design' />
-        <Skill x='0vw' y='-22vw' text='Figma' />
+        <Skill x='30vw' y='-5vw' text='Git' />
         <Skill x='-20vw' y='18vw' text='Redux' />
         <Skill x='18vw' y='18vw' text='Tailwind CSS' />
+        <Skill x='0vw' y='22vw' text='React Testing Library' />
+        <Skill x='-18vw' y='11vw' text='Jest' />
+        {/* cloud
+        docker
+         */}
       </div>
     </>
   )
