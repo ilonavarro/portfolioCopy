15f440fc92a9fe4c730efb032d4216b7b8ecdce2
ilonavarro <ilo.navarro+github@gmail.com>
2023-06-01 02:23:19 +0100

    updates and building contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -14,64 +14,95 @@ export default function Contact() {
         <div className='flex flex-col items-center'>
           <form>
             <div className='w-full flex flex-col pb-4'>
-              <label htmlFor='name'>Name</label>
-              <input
-                type='text'
-                name='name'
-                id='name'
-                placeholder='Your name here'
-                className='border border-solid border-dark rounded px-2 bg-light w-full'
-              />
-            </div>
-            <div className='w-full flex flex-col pb-4'>
-              <label htmlFor='email'>Email</label>
-              <input
-                type='email'
-                name='email'
-                id='email'
-                placeholder='Your best email here'
-                className='border border-solid border-dark rounded px-2 bg-light w-full'
-              />
-            </div>
-            <label htmlFor='message'>How can I create value for you?</label>
-            <div>
-              <textarea
-                rows={4}
-                name='message'
-                placeholder='Your message here'
-                className='border border-solid border-dark rounded px-2 bg-light w-full'
-              />
-            </div>
-
-            <label
-              htmlFor='email'
-              // class='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-              className='block mb-1 text-dark'
-            >
-              Your Email
-            </label>
-            <div className='relative'>
-              <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-                <svg
-                  aria-hidden='true'
-                  // class='w-5 h-5 text-gray-500 dark:text-gray-400'
-                  className='w-5 h-5 text-dark'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path d='M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z'></path>
-                  <path d='M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z'></path>
-                </svg>
-              </div>
-              <input
-                type='text'
-                id='email'
-                // class='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                className='border border-dark bg-light text-dark text-sm rounded-lg 
+              <label
+                htmlFor='name'
+                // class='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                className='block mb-1 text-dark'
+              >
+                Name
+              </label>
+              <div className='relative'>
+                <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    xmlSpace='preserve'
+                    viewBox='0 0 52 52'
+                    width='1em'
+                    height='1em'
+                  >
+                    <path d='M50 43v2.2c0 2.6-2.2 4.8-4.8 4.8H6.8C4.2 50 2 47.8 2 45.2V43c0-5.8 6.8-9.4 13.2-12.2l.6-.3c.5-.2 1-.2 1.5.1 2.6 1.7 5.5 2.6 8.6 2.6s6.1-1 8.6-2.6c.5-.3 1-.3 1.5-.1l.6.3C43.2 33.6 50 37.1 50 43zM26 2c6.6 0 11.9 5.9 11.9 13.2S32.6 28.4 26 28.4s-11.9-5.9-11.9-13.2S19.4 2 26 2z' />
+                  </svg>
+                </div>
+                <input
+                  type='text'
+                  id='name'
+                  placeholder='Your Name'
+                  // class='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='border border-dark bg-light text-dark text-sm rounded-lg 
                 block w-full pl-10 p-2.5'
-                placeholder='user@website'
-              />
+                />
+              </div>
+              <label
+                htmlFor='email'
+                // class='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                className='block mb-1 mt-4 text-dark'
+              >
+                Email
+              </label>
+              <div className='relative'>
+                <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    xmlSpace='preserve'
+                    viewBox='0 0 52 52'
+                    width='1em'
+                    height='1em'
+                  >
+                    <path d='M24.9 30.1c.6.6 1.5.6 2.1 0l22.6-21c.4-.8.3-2.1-1.3-2.1l-44.7.1c-1.2 0-2.2 1.1-1.3 2.1l22.6 20.9z' />
+                    <path d='M50 17.3c0-1-1.2-1.6-2-.9L30.3 32.7c-1.2 1.1-2.7 1.7-4.3 1.7s-3.1-.6-4.3-1.6L4.1 16.4c-.8-.7-2-.2-2 .9C2 17 2 40 2 40c0 2.2 1.8 4 4 4h40c2.2 0 4-1.8 4-4V17.3z' />
+                  </svg>
+                </div>
+                <input
+                  type='email'
+                  id='email'
+                  placeholder='your@email'
+                  // class='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='border border-dark bg-light text-dark text-sm rounded-lg 
+                block w-full pl-10 p-2.5'
+                />
+              </div>
+
+              {/* textarea */}
+              <label
+                htmlFor='message'
+                // class='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                className='block mb-1 mt-4 text-dark'
+              >
+                How can I create value for you?
+              </label>
+              <div className='relative'>
+                <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    xmlSpace='preserve'
+                    viewBox='0 0 52 52'
+                    width='1em'
+                    height='1em'
+                  >
+                    <path d='M26 4C12.7 4 2.1 13.8 2.1 25.9c0 3.8 1.1 7.4 2.9 10.6.3.5.4 1.1.2 1.7l-3.1 8.5c-.3.8.5 1.5 1.3 1.3l8.6-3.3c.5-.2 1.1-.1 1.7.2 3.6 2 7.9 3.2 12.5 3.2C39.3 48 50 38.3 50 26.1 49.9 13.8 39.2 4 26 4zM14 30c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4zm12 0c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4zm12 0c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z' />
+                  </svg>
+                </div>
+                <textarea
+                  rows={4}
+                  id='message'
+                  placeholder='Your message here'
+                  // class='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='border border-dark bg-light text-dark text-sm rounded-lg 
+                block w-full pl-10 p-2.5'
+                />
+              </div>
+
+              {/* end textarea */}
             </div>
 
             <button type='submit'>Send</button>
