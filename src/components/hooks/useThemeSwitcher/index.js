880383f2998e4026e70def9a88769a2f67c8dbe4
ilonavarro <ilo.navarro+github@gmail.com>
2023-06-17 20:49:35 +0100

    dark mode (I need to finish this)

--- src/components/hooks/useThemeSwitcher/index.js
+++ src/components/hooks/useThemeSwitcher/index.js
@@ -1,8 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function useThemeSwitcher() {
   const preferDarkQuery = '(prefers-color-scheme: dark)'
   const [mode, setMode] = useState('')
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(preferDarkQuery)
+    const userPref = window.localStorage.getItem('theme')
+
+    const handleChange = () => {
+      if (userPref) {
+        let check = userPref === 'dark' ? 'dark' : 'light'
+        setMode(check)
+      }
+    }
+
+    mediaQuery.addEventListener('change', handleChange)
+  }, [])
 
   return <div>useThemeSwitcher</div>
 }
