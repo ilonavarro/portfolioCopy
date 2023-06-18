a40bdf82368e79954aac082d86fecd6d81bcec9a
ilonavarro <ilo.navarro+github@gmail.com>
2023-06-18 22:52:20 +0100

    updates and Dark Mode Almost done

--- src/components/hooks/useThemeSwitcher/index.js
+++ src/components/hooks/useThemeSwitcher/index.js
@@ -9,13 +9,34 @@ export default function useThemeSwitcher() {
     const userPref = window.localStorage.getItem('theme')
 
     const handleChange = () => {
+      let check = userPref
+        ? userPref === 'dark'
+          ? 'dark'
+          : 'light'
+        : mediaQuery.matches
+        ? 'dark'
+        : 'light'
+      setMode(check)
+      check === 'dark'
+        ? document.documentElement.classList.add('dark')
+        : document.documentElement.classList.remove('dark')
+
+      /*
       if (userPref) {
         let check = userPref === 'dark' ? 'dark' : 'light'
         setMode(check)
+        check === 'dark'
+          ? document.documentElement.classList.add('dark')
+          : document.documentElement.classList.remove('dark')
+      } else {
+        let check = mediaQuery.matches ? 'dark' : 'light'
       }
+      */
     }
 
     mediaQuery.addEventListener('change', handleChange)
+
+    return () => mediaQuery.removeEventListener('change', handleChange)
   }, [])
 
   return <div>useThemeSwitcher</div>
